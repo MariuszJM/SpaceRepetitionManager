7fb6b95cc9316d4641f8f9dae81cd4f5dd784ecd
MariuszMJ <mariusz.michna.j@gmail.com>
2023-10-19 13:19:44 +0200

    authenticate only ones

--- src/google_calendar.py
+++ src/google_calendar.py
@@ -1,17 +1,34 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request  # Dodane
+import os
+import pickle
 from datetime import datetime, timezone
-
 
 class GoogleCalendar:
     def __init__(self, calendar_id, credentials_file):
         self.calendar_id = calendar_id
-        flow = InstalledAppFlow.from_client_secrets_file(
-            credentials_file,
-            scopes=['https://www.googleapis.com/auth/calendar']
-        )
-        credentials = flow.run_local_server(port=0)
-        self.service = build('calendar', 'v3', credentials=credentials)
+        self.credentials = None
+        self.service = None
+
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                self.credentials = pickle.load(token)
+
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())  # Poprawione
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    credentials_file,
+                    scopes=['https://www.googleapis.com/auth/calendar']
+                )
+                self.credentials = flow.run_local_server(port=0)
+
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.credentials, token)
+
+        self.service = build('calendar', 'v3', credentials=self.credentials)
 
     def create_event(self, summary, description, start_time, end_time):
         event = {
