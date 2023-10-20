94764b5d3c662f74e03403c65ee9bdb062c9c3bd
MariuszMJ <mariusz.michna.j@gmail.com>
2023-10-20 15:25:37 +0200

    Undoing events with remowving used history files with asking - done

--- main_undo_last_n.py
+++ main_undo_last_n.py
@@ -1,6 +1,6 @@
 from src.google_calendar import GoogleCalendar
 from src.task_scheduler import TaskScheduler
-from src.utils import get_last_n_files  # Import utility function
+from src.utils import get_last_n_files, display_aggregated_undo_changes  # Import utility functions
 
 CREDENTIALS_FILE = 'credentials.json'
 CALENDAR_ID = 'mariusz.michna.j@gmail.com'
@@ -11,10 +11,20 @@ def main():
     google_calendar = GoogleCalendar(calendar_id=CALENDAR_ID, credentials_file=CREDENTIALS_FILE)
     task_scheduler = TaskScheduler(CONFIG_FILE, google_calendar)
 
-    last_n_files = get_last_n_files(LAST_N)  # Use utility function
-    for history_file in last_n_files:
-        print(f"Undoing changes from file: {history_file}")
-        task_scheduler.undo_added_tasks(history_file.replace('.yaml', ''))
+    last_n_files = get_last_n_files(LAST_N)
+
+    print("Spodziewane zmiany:")
+    display_aggregated_undo_changes(last_n_files)
+
+    choice = input("Czy chcesz zatwierdzić te zmiany? (t/n): ").strip().lower()
+
+    if choice == 't':
+        for history_file in last_n_files:
+            print(f"Undoing changes from file: {history_file}")
+            task_scheduler.undo_added_tasks(history_file.replace('.yaml', ''))
+        print("Zmiany zostały wprowadzone.")
+    else:
+        print("Zmiany nie zostały wprowadzone.")
 
 if __name__ == "__main__":
     main()
--- main_undo_to_datetime.py
+++ main_undo_to_datetime.py
@@ -1,22 +1,31 @@
 from datetime import datetime
 from src.google_calendar import GoogleCalendar
 from src.task_scheduler import TaskScheduler
-from src.utils import get_files_before_datetime
+from src.utils import get_files_before_datetime, display_aggregated_undo_changes  # Import utility functions
 
-# Global variables
 CREDENTIALS_FILE = 'credentials.json'
 CALENDAR_ID = 'mariusz.michna.j@gmail.com'
 CONFIG_FILE = 'config.yaml'
-TARGET_DATETIME = datetime(2023, 10, 20, 12, 37)
+TARGET_DATETIME = datetime(2023, 10, 20, 14, 30)
 
 def main():
     google_calendar = GoogleCalendar(calendar_id=CALENDAR_ID, credentials_file=CREDENTIALS_FILE)
     task_scheduler = TaskScheduler(CONFIG_FILE, google_calendar)
 
     history_files_before_datetime = get_files_before_datetime(TARGET_DATETIME)
-    for history_file in history_files_before_datetime:
-        print(f"Undoing changes from file: {history_file}")
-        task_scheduler.undo_added_tasks(history_file.replace('.yaml', ''))
+
+    print("Spodziewane zmiany:")
+    display_aggregated_undo_changes(history_files_before_datetime)
+
+    choice = input("Czy chcesz zatwierdzić te zmiany? (t/n): ").strip().lower()
+
+    if choice == 't':
+        for history_file in history_files_before_datetime:
+            print(f"Undoing changes from file: {history_file}")
+            task_scheduler.undo_added_tasks(history_file.replace('.yaml', ''))
+        print("Zmiany zostały wprowadzone.")
+    else:
+        print("Zmiany nie zostały wprowadzone.")
 
 if __name__ == "__main__":
     main()
